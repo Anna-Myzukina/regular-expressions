9a4780ccf6606be319aeaa6fe45d4c20c7eb32f6
Ann <anna.muzykina83@gmail.com>
2019-01-16 12:11:24 +0200

    Update matchBeginningStringPatterns.js

--- Regular Expressions/matchBeginningStringPatterns.js
+++ Regular Expressions/matchBeginningStringPatterns.js
@@ -16,6 +16,6 @@ firstRegex.test(notFirst);
 
 Use the caret character in a regex to find "Cal" only in the beginning of the string rickyAndCal.*/
 
-let rickyAndCal = "Cal and Ricky both like racing.";
-let calRegex = /^Cal/; // Change this line
-let result = calRegex.test(rickyAndCal);
+        let rickyAndCal = "Cal and Ricky both like racing.";
+        let calRegex = /^Cal/; // Change this line
+        let result = calRegex.test(rickyAndCal);
